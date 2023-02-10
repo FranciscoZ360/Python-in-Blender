e80d271cdc8d8f6e00b156270f5faeccbe0968b9
Julio Francisco Zerpa Mayor <35926348+FranciscoZ360@users.noreply.github.com>
2023-02-10 14:26:10 -0300

    Update Primitive_Uv_Sphere.py

--- Primitive_Uv_Sphere.py
+++ Primitive_Uv_Sphere.py
@@ -1,16 +1,20 @@
+#----------------PARTE 1-------------------------#
+# CREAR UNA ESFERA
+
 import bpy
 
+#creamos la funcion de crear una esfera
 def create_uv_sphere():
-    # Eliminar cualquier objeto existente en la escena
+    # Seleccionamos todos los objetos presente en la escena y los eliminamos.
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete(use_global=False)
     
-    # Crear un nuevo cubo y seleccionarlo
+   # Creamos la nueva esfera y la serelcionamos
     bpy.ops.mesh.primitive_uv_sphere_add(location=(0,0,0))
     sphere = bpy.context.object
     sphere.name = "Sphere"
     sphere.scale = (2, 2, 2)
     sphere.select_set(True)
 
-# Ejecutar la función para crear un cubo
+# Ejecutamos la funcion de crear una esfera
 create_uv_sphere()
